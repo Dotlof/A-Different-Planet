d4395250e04f9b4fbb8297b4af78dcd4b0764c34
ErloZim <70133951+ErloZim@users.noreply.github.com>
2020-10-09 13:15:24 +0200

    Shoot TopDown

---
+++ Assets/Scripts/TopDown/scr_Laser.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class scr_Laser : MonoBehaviour
+{
+
+    public float Direction;
+    float speed = 100;
+    Vector3 moveRight = new Vector3(1, 0, 0);
+    Vector3 moveLeft = new Vector3(-1, 0, 0);
+    Vector3 moveTop = new Vector3(0, 1, 0);
+    Vector3 moveBottom = new Vector3(0, -1, 0);
+
+    IEnumerator kill()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(kill());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Direction == 1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.position += moveRight * speed * Time.deltaTime;
+        }
+        if (Direction == 2)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.position += moveLeft * speed * Time.deltaTime;
+        }
+        if (Direction == 3)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, -90);
+            transform.position += moveTop * speed * Time.deltaTime;
+        }
+        if (Direction == 4)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+            transform.position += moveBottom * speed * Time.deltaTime;
+        }
+
+
+    }
+}
--- Assets/Scripts/TopDown/scr_Starship.cs
+++ Assets/Scripts/TopDown/scr_Starship.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class scr_Starship : MonoBehaviour
 {
+    public void Shoot()
+    {
+        Projectile.GetComponent<scr_Laser>().Direction = Direction;
+        Instantiate(Projectile, this.transform.position, Quaternion.identity);
+    }
+
 
     Vector3 movement;
     public float MovementSpeed = 2;
+    public int Direction;
+    public GameObject Projectile;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +30,45 @@ public class scr_Starship : MonoBehaviour
 
         movement.y = Input.GetAxisRaw("Vertical");
         transform.position += new Vector3(0, movement.y, 0) * Time.deltaTime * MovementSpeed;
+
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Direction = 1;
+            transform.localScale = new Vector3(-1, 1, 1);
+            this.transform.localRotation = Quaternion.Euler(0, 0, 45);
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                this.transform.localRotation = Quaternion.Euler(0, 0, 45);
+                Direction = 2;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.localRotation = Quaternion.Euler(0, 0, -45);
+            Direction = 3;
+        }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                this.transform.localRotation = Quaternion.Euler(0, 0, 135);
+                Direction = 4;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Shoot();
+        }
     }
 }
