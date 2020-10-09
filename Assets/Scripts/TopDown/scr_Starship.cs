8086a2969df6719a1710e9821d4a35047b88e6fc
ErloZim <70133951+ErloZim@users.noreply.github.com>
2020-10-09 12:44:17 +0200

    prefab und char

---
+++ Assets/Scripts/TopDown/scr_Starship.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Starship : MonoBehaviour
+{
+
+    Vector3 movement;
+    public float MovementSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movement.x, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        movement.y = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(0, movement.y, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
