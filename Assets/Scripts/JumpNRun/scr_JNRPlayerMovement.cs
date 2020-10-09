ccedb56f89135710c1c4ac83797f6d4da4cf5a54
Dotlof <51374399+Dotlof@users.noreply.github.com>
2020-10-09 12:15:23 +0200

    Added Folders

--- Assets/Scripts/JumpNRun/scr_JNRPlayerMovement.cs
+++ Assets/Scripts/JumpNRun/scr_JNRPlayerMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class scr_JNRPlayerMovement : MonoBehaviour
 {
+    public GameObject Player;
+    public Rigidbody2D rb;
+    public float MoveSpeed = 20F;
+    public float JumpHeight = 5f;
+    float direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,12 @@ public class scr_JNRPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        direction = Input.GetAxisRaw("Horizontal");
+
+        if(direction == 0 || direction > 0)
+        {
+            Player.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else Player.transform.localScale = new Vector3(1, 1, 1);
     }
 }
